f3588c5ee231038bc0ca2df15a73baf6f901bfbf
Jason Begleiter <jason@meebo-inc.com>
2011-09-21 16:09:11 +0000

    Updated

--- quantcast/scrape.py
+++ quantcast/scrape.py
@@ -1,10 +1,8 @@
 ## Quantcast Scraping Module
 ## 9/15/11
 
-import os
-import re
-import sys
-import urllib
+import os, re, sys, urllib
+
 
 ##Parse inbound Text file of sites
 def open_file(filename):
@@ -14,6 +12,16 @@ def open_file(filename):
 		site_list.append(site.rstrip('\n'))
 	f.close()
 	return site_list
+
+##Make sure HTML is formatted properly
+def good_html(url_file):
+	info = url_file.info()
+	if info.gettype() == 'text/html': 
+		utext = ufile.read()
+		return utext
+	else: 
+		print 'Error - dis ain\'t formatted correctly'
+		sys.exit(1)
 
 
 ##Contruct queries, ping quantcast, retrieve data, clean
@@ -35,7 +43,7 @@ def quant(site_list):
 		#print str(utext.index('Audience Also Likes')) + ' finish'
 		
 		##Retrieve relevant substring
-		utext_sub = utext[utext.index('US Demographics'):utext.index('<h4>Audience Also Likes')] #start:end
+		utext_sub = utext[utext.index('id="demographic-tooltip"'):utext.index('id="simaudience-tooltip"')]
 		utext_sub = utext_sub[utext_sub.index('<tbody>'):]
 		param = {}
 		
@@ -46,19 +54,56 @@ def quant(site_list):
 		
 		stats[url] = param
 
-	for stat in stats:
-		print stat, stats[stat]
-	#return stats
+	return stats
 
-		
-		
 
+##Lifestyle Numbers
+def LS_quant(site_list):
+
+	stats = {}
+	for url in site_list:
+		ufile = urllib.urlopen('http://www.quantcast.com/' + url +'/lifestyle')
+
+		utext = good_HTML(ufile)		
+		utext_sub = utext[utext.index('<p class="caption">Data Source: United States Monthly</p>'):utext.index('<div id="footer">')] 
+		param = {}
+		
+		cat_tuples = re.findall(r'<td>([\w\d\+\.\s/-]+)</td>\n<td class="digit">([\d\.+]+)x</td>',utext_sub)
+		for tuple in cat_tuples:
+			param[tuple[0]] = tuple[1]
+			
+		stats[url] = param
+		return stats
+
+
+##Reach Statistics		
+def Reach_quant(site_list):
+	stats = {}
+	for url in site_list:
+		ufile = urllib.urlopen('http://www.quantcast.com/' + url)
+		info = ufile.info()
+		
+		##Ensures it's a text-based page
+		if info.gettype() == 'text/html': 
+			utext = ufile.read()
+		else: 
+			print 'Error - shit ain\'t formatted correctly'
+			sys.exit(1)
+		
+		utext_sub = utext[utext.index('id="reach-wd'):utext.index('id="sparkline-wd:')]
+		tuple = re.findall(r'">\n([\d\w\.]+)<p class="label">',utext_sub)
+		
+		stats[url] = tuple
+	#print stats
+	return stats
 
 def main():
 	args = sys.argv[1:]
 	for filename in args:
 		list_main = open_file(filename)
-		quant(list_main)
+		stats_main = quant(list_main)
+		stats_lifestyle = LS_quant(list_main)
+		Reach_quant(list_main)
 
 if __name__ == '__main__':
   main()
